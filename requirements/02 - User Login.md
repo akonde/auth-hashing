67e5757cdb2e2094289b6e292ccf1bcbe3c72415
Akintayo Durotoye <tayodurotoye@gmail.com>
2024-12-11 13:53:33 +0000

    updated

--- requirements/02 - User Login.md
+++ requirements/02 - User Login.md
@@ -1,4 +1,4 @@
-# User Login
+id# User Login
 
 1. Implement the controller function in `src/routers/sessions.js` to compare the password provided in the request body to the hashed password in the database, then create a JWT if the login was successful.
 
--- src/routers/sessions.js
+++ src/routers/sessions.js
@@ -5,18 +5,41 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const prisma = require('../utils/prisma.js')
 
+const APP_SECRET = process.env.JWT_SECRET;
+
 router.post('/', async (req, res) => {
-    const { username, password } = req.body;
     // Get the username and password from the request body
+    const { username, password } = req.body;
 
     // Check that a user with that username exists in the database
-    // Use bcrypt to check that the provided password matches the hashed password on the user
-    // If either of these checks fail, respond with a 401 "Invalid username or password" error
+    const user = await prisma.user.findFirst({
+        where: {
+          username: username,
+        },
+        select: {
+          id: true,
+          password: true
+        }
+      })
 
+      if (!user) {
+         return res.status(401).json({ error: 'Invalid username or password!' });
+      }
+    // Use bcrypt to check that the provided password matches the hashed password on the user
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    // If either of these checks fail, respond with a 401 "Invalid username or password" error
+    if (!validPassword) {
+        return res.status(401).json({ error: 'Invalid username or password' });
+    }
+    
     // If the user exists and the passwords match, create a JWT containing the username in the payload
     // Use the JWT_SECRET environment variable for the secret key
-
+  const token = jwt.sign({ id: user.id}, APP_SECRET);
+  
+    
     // Send a JSON object with a "token" key back to the client, the value is the JWT created
+    res.json({token: token})
 });
 
 module.exports = router;
